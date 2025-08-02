9afc17c4fa14985ca70d3794d4eb3f21bd7ca859
Evangelinangel <158428930+Evangelinangel@users.noreply.github.com>
2025-08-02 12:32:53 +0530

    Add files via upload

---
+++ Frontend Test Submission/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import URLShortener from './components/url';
+import Stats from './components/stats';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<URLShortener />} />
+      <Route path="/stats" element={<Stats />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
---
+++ Frontend Test Submission/validators.js
@@ -0,0 +1,9 @@
+export const isValidURL = (url) => {
+  const pattern = /^(https?:\/\/)?([\w\d-]+\.)+\w{2,}(\/.*)?$/;
+  return pattern.test(url);
+};
+
+export const isValidShortcode = (code) => {
+  const pattern = /^[a-zA-Z0-9]{4,}$/;
+  return pattern.test(code);
+};
